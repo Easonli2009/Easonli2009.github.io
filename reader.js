711379ce6f29997becf03cb2bbf1269a8abcb23c
Easonli2009 <106458895+Easonli2009@users.noreply.github.com>
2022-12-07 16:15:35 +0800

    Update reader.js

--- reader.js
+++ reader.js
@@ -119,7 +119,7 @@ export function readZip(result, {
 	const tl = urls['jszip'].reverse()[0];
 	if (!self._zip_worker) {
 		onloadstart();
-		const worker = new Worker(`./zip.js#${tl}`); //以后考虑indexedDB存储url
+		const worker = new Worker(`./zip.js`); //以后考虑indexedDB存储url
 		let total = 0;
 		worker.addEventListener('message', async msg => {
 			/** @type {{data:{name:string,path:string,buffer:ArrayBuffer},total:number}} */
